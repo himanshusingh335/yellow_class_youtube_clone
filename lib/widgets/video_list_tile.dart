19d4975056072192d015a92d6358b7d37b4eb282
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2022-07-01 15:16:20 +0530

    ♻️ code refactoring

--- lib/widgets/video_list_tile.dart
+++ lib/widgets/video_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 import '../models/my_media.dart';
 
@@ -12,34 +13,41 @@ class VideoListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 350.0,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(listData.coverPicture!),
-                  fit: BoxFit.cover),
+    return VisibilityDetector(
+      key: const Key('Container'),
+      onVisibilityChanged: (VisibilityInfo info) {
+        var visiblePercentage = info.visibleFraction * 100;
+        debugPrint('Widget ${info.key} is $visiblePercentage% visible');
+      },
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(listData.coverPicture!),
+                    fit: BoxFit.cover),
+              ),
             ),
-          ),
-          ListTile(
-            contentPadding: const EdgeInsets.all(8.0),
-            dense: true,
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(listData.coverPicture!),
+            ListTile(
+              contentPadding: const EdgeInsets.all(8.0),
+              dense: true,
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(listData.coverPicture!),
+              ),
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 4.0),
+                child: Text(listData.title!),
+              ),
+              subtitle: Text("${listData.title} . 20000 . 25/6/2022"),
+              trailing: Container(
+                  margin: const EdgeInsets.only(bottom: 20.0),
+                  child: const Icon(Icons.more_vert)),
             ),
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 4.0),
-              child: Text(listData.title!),
-            ),
-            subtitle: Text("${listData.title} . 20000 . 25/6/2022"),
-            trailing: Container(
-                margin: const EdgeInsets.only(bottom: 20.0),
-                child: const Icon(Icons.more_vert)),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
