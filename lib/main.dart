d6edb527d13e240661cb63b28b94ca49da93e714
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2022-07-07 07:40:20 +0530

    🔥 remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:yellow_class_youtube_clone/models/my_media.dart';
 import 'package:yellow_class_youtube_clone/screens/navigation_screen.dart';
-import 'package:yellow_class_youtube_clone/services/fetch_video_list.dart';
 
 void main() {
   runApp(const YellowClassYoutube());
@@ -13,7 +11,6 @@ class YellowClassYoutube extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    List<MyMedia> data = getList(jsonStringList);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.yellow),
